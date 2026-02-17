4e8eb57ac5c19e669e84557e27f48fa25caf1218
Nezeroffh07 <nzrvonurlan3@gmail.com>
2026-02-17 21:23:56 +0000

    Fix overflow and NaN cases for add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,9 +3,19 @@
 
 
 def add_integer(a, b=98):
-    """Adds two integers."""
+    """
+    Adds 2 integers.
+    a and b must be integers or floats.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    # NaN və ya Sonsuzluq (Overflow) yoxlaması
+    if a != a or abs(a) > 1.7976931348623157e+308:
+        raise TypeError("a must be an integer")
+    if b != b or abs(b) > 1.7976931348623157e+308:
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
