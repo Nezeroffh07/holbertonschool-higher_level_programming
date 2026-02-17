51293aa0faf16fe935ef8a734437c2123a34b87a
Nezeroffh07 <nzrvonurlan3@gmail.com>
2026-02-17 20:36:39 +0000

    Add 102-complex_delete.py

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    """Deletes keys with a specific value in a dictionary."""
+    # Lüğət üzərində gəzərkən onu dəyişdirmək xəta verir.
+    # Ona görə də əvvəlcə açarların surətini (list kimi) götürürük.
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
