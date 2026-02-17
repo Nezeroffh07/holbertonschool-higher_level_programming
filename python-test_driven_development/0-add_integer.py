e94f5f9cfc781784785ed051b83eb577e0c9b979
Nezeroffh07 <nzrvonurlan3@gmail.com>
2026-02-17 21:29:33 +0000

    Handle edge cases: NaN, Infinity, and missing arguments

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,15 +5,15 @@
 def add_integer(a, b=98):
     """
     Adds 2 integers.
-    a and b must be integers or floats.
+    a and b must be first casted to integers if they are float.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    # NaN və ya Infinity (Overflow) yoxlaması
-    # Python-da NaN öz-özünə bərabər olmayan tək dəyərdir (a != a)
+    # NaN və ya Sonsuzluq (Overflow) yoxlaması
+    # Python-da NaN-ı yoxlamağın ən qısa yolu: x != x
     if a != a or a == float('inf') or a == float('-inf'):
         raise TypeError("a must be an integer")
     if b != b or b == float('inf') or b == float('-inf'):
