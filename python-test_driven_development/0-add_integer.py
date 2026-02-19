62ce3250c2671ce3cc2b678aaeaf06bf105f2f8b
Nezeroffh07 <nzrovnurlan3@gmail.com>
2026-02-19 20:00:19 +0000

    Add missing test cases for NaN, overflow and arguments

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-"""Module for add_integer method."""
+"""
+Module that provides a function to add two integers.
+"""
 
 
 def add_integer(a, b=98):
-    """Adds two integers."""
-    if type(a) not in [int, float]:
+    """
+    Adds two numbers.
+
+    a and b must be integers or floats.
+    Floats are casted to integers before addition.
+    """
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
