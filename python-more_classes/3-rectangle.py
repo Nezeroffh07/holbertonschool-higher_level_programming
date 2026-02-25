a1754910c8caad5723bb6685384efbda5f91624c
Nezeroffh07 <nzrovnurlan3@gmail.com>
2026-02-25 13:03:32 +0000

    Fix pycodestyle W293 in 3-rectangle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -55,12 +55,12 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
-        
+
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+
         rect = []
         for i in range(self.__height):
             rect.append("#" * self.__width)
