fae44c879a2c094900158bfbcb3800c0da5f7182
Nezeroffh07 <nzrovnurlan3@gmail.com>
2026-02-25 14:30:23 +0000

    Fix pycodestyle E501 in 4-inherits_from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,8 @@ a class that inherited (directly or indirectly) from a specified class.
 
 
 def inherits_from(obj, a_class):
-    """Checks if an object is an instance of a class that inherited from a_class.
+    """Checks if an object is an instance of a class that inherited
+    from a_class.
 
     Args:
         obj: The object to check.
@@ -15,4 +16,4 @@ def inherits_from(obj, a_class):
     Returns:
         True if obj is an instance of a subclass of a_class, otherwise False.
     """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class`
