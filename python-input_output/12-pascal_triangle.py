17fcd67a9976b1be091ba62325dc13f576ae0128
Nezeroffh07 <nzrovnurlan3@gmail.com>
2026-03-06 21:05:37 +0000

    Fix PEP8 formatting

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,24 +6,21 @@ This module defines a function that generates Pascal's triangle.
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal's triangle of n.
+    Returns a list of lists of integers representing Pascal's triangle.
     """
     if n <= 0:
         return []
 
-    # Üçbucağı ən təpədəki ilk sətirlə (1) başladırıq
     triangle = [[1]]
 
-    # 1-ci sətirdən başlayaraq n-ci sətirə qədər dövr edirik
     for i in range(1, n):
-        row = [1]  # Hər yeni sətir 1 ilə başlayır
-        prev_row = triangle[-1]  # Bir əvvəlki sətri götürürük
-        
-        # Əvvəlki sətrin elementlərini cüt-cüt toplayıb yeni sətirə əlavə edirik
+        row = [1]
+        prev_row = triangle[-1]
+
         for j in range(len(prev_row) - 1):
             row.append(prev_row[j] + prev_row[j + 1])
-            
-        row.append(1)  # Hər yeni sətir 1 ilə bitir
-        triangle.append(row)  # Hazır olan sətri əsas üçbucağa əlavə edirik
+
+        row.append(1)
+        triangle.append(row)
 
     return triangle
