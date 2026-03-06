e86ee821bd7d565769db2afb89057332b456537f
Nezeroffh07 <nzrovnurlan3@gmail.com>
2026-03-06 20:31:01 +0000

    Fix shebang line

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,10 @@
-3-to_json_string.py
+#!/usr/bin/python3
+"""
+This module defines a string-to-JSON function.
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)."""
+    return json.dumps(my_obj)
