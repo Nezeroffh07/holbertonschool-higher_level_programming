9868003bec7cb4bfdbcbe67332afce98e88b2fa9
Nezeroffh07 <nzrovnurlan3@gmail.com>
2026-03-13 17:25:11 +0000

    Fix typo in comment at the beginning of the file

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
-reates a table called first_table with id (INT) and name (VARCHAR(256))
+-- Creates a table called first_table with id (INT) and name (VARCHAR(256))
 CREATE TABLE IF NOT EXISTS first_table (
-	    id INT,
-	    name VARCHAR(256)
-	);
+    id INT,
+    name VARCHAR(256)
+);
