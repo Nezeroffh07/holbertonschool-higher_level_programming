bf917a2133ae4fc0e34d7aa672fc7516f12fe2bd
Nezerofh07 <nzrovnurlan3@gmail.com>
2026-03-14 02:14:21 +0400

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,13 +7,10 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Terminaldan 4 arqumenti alırıq
     mysql_user = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
     state_searched = sys.argv[4]
-
-    # Bazaya qoşuluruq
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -21,24 +18,11 @@ if __name__ == "__main__":
         passwd=mysql_password,
         db=db_name
     )
-
-    # Cursor yaradırıq
     cursor = db.cursor()
-
-    # .format() istifadə edərək sorğunu qururuq
-    # BINARY burada hərfin böyük-kiçikliyinə (case-sensitive) diqqət edir
     query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_searched)
-    
-    # Sorğunu icra edirik
     cursor.execute(query)
-
-    # Nəticələri götürürük
     rows = cursor.fetchall()
-
-    # Çap edirik
     for row in rows:
         print(row)
-
-    # Bağlantıları bağlayırıq
     cursor.close()
     db.close()
