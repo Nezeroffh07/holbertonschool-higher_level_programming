c53e6ad1c2edcd569d80c8161656067827a01a7c
Nezerofh07 <nzrovnurlan3@gmail.com>
2026-03-14 02:13:12 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,10 @@ Lists all states from the database hbtn_0e_0_usa.
 """
 import MySQLdb
 import sys
-
 if __name__ == "__main__":
-    # Get arguments from command line
     mysql_user = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
-
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -19,20 +15,10 @@ if __name__ == "__main__":
         passwd=mysql_password,
         db=db_name
     )
-
-    # Create a cursor object
     cursor = db.cursor()
-
-    # Execute the SQL query
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all the results
     rows = cursor.fetchall()
-
-    # Print each row
     for row in rows:
         print(row)
-
-    # Close cursor and database connection
     cursor.close()
     db.close()
