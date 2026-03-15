35f0270874e550c65faeea4dfc4b220c21340fd9
Nezeroffh07 <nzrovnurlan3@gmail.com>
2026-03-15 11:06:59 +0000

    salam

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
+"""Script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument safe from injection"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_searched = sys.argv[4]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_user,
-        passwd=mysql_password,
-        db=db_name
-    )
-
-    cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_searched,))
-
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
+        db = MySQLdb.connect(
+                        host="localhost",
+                                port=3306,
+                                        user=sys.argv[1],
+                                                passwd=sys.argv[2],
+                                                        db=sys.argv[3]
+                                                            )
+            cur = db.cursor()
+                cur.execute(
+                                "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                                        (sys.argv[4],)
+                                            )
+                    for row in cur.fetchall():
+                                print(row)
+                                    cur.close()
+                                        db.close()
