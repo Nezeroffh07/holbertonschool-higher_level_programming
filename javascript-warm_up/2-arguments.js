5c34765ea5bb41c342ec98644f59bb743afda966
Nezeroffh07 <nzrovnurlan3@gmail.com>
2026-03-28 10:42:15 +0000

    Add script that handles process arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+cat > 2-arguments.js << 'EOF'
+#!/usr/bin/node
+const count = process.argv.length;
+
+if (count === 2) {
+  console.log('No argument');
+} else if (count === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
